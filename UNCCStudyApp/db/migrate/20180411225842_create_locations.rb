59b31825ea7d6d161855e24ea5c7fd614ce0e352
Andrew Davis <adavi@uncc.edu>
2018-04-11 23:07:04 +0000

    Added Locations and Building models and controllers

--- UNCCStudyApp/db/migrate/20180411225842_create_locations.rb
+++ UNCCStudyApp/db/migrate/20180411225842_create_locations.rb
@@ -1,12 +1,10 @@
 class CreateLocations < ActiveRecord::Migration[5.1]
   def change
     create_table :locations do |t|
-      t.string :building
       t.string :floor
-      t.float :latitude
-      t.float :longitude
-      t.string :room_number
+      t.string :roomNumber
       t.string :description
+      t.reference :building
 
       t.timestamps
     end
