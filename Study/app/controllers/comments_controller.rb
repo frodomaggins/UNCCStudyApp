480a89587afeb4642f3da297ef0d7294d1b7eeac
Andrew Davis <adavi@uncc.edu>
2018-04-22 17:20:40 +0000

    Delete functionality for comments

--- Study/app/controllers/comments_controller.rb
+++ Study/app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
       redirect_to location_path(@location)
   end
   
+  def destroy
+      @location = Location.find(params[:location_id])
+      @comment = @location.comments.find(params[:id])
+      @comment.destroy
+      redirect_to location_path(@location)
+  end
+  
   private
       def comment_params
         params.require(:comment).permit(:commenter, :body)
