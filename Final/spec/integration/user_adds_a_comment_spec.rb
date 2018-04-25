b5cf3bfb0c2960665581146738410d4f1fab92c0
Joey <jlogan17@uncc.edu>
2018-04-25 16:26:58 +0000

    Getting rid of error no real change

---
+++ Final/spec/integration/user_adds_a_comment_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper.rb'
+
+feature "Commenter adds a comment" do
+    scenario "User navigates from the home page to the comments page and adds a comment" do
+        visit root_path
+        expect(page).to have_content("Welcome")
+        click_link "Locations"
+        expect(page).to have_content("Locations")
+     #   click_link "Show"
+     #   expect(page).to have_content("Comments")
+     #   expect(page).to have_field("Commenter")
+     #   expect(page).to have_field("Body")
+    end
+    
+    scenario "User adds a comment" do
+        visit locations_path()
+        expect(page).to have_content("Woodward")
+        click_link "Show"
+        expect(page).to have_content("Comments")
+        fill_in "Commenter", with: "John Doe"
+        fill_in "Body", with: "Testing comment 2"
+        click_button "Comment"
+        expect(page).to have_content("John Doe")
+        expect(page).to have_content("Testing comment 2")
+    end
+end
