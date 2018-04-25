61def6ee1d08e2d4b982231030065f2f83610021
frodomaggins <mreichar@uncc.edu>
2018-04-25 17:40:11 +0000

    added some locations to seeds.rb

--- Final/db/seeds.rb
+++ Final/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    Location.create(:name=>"Woodward", :latitude=>1.1, :longitude=>1.1, :floor=>"1", :roomNumber=>"100", :description=>"This is a test location")
+    Location.create(:name=>"Woodward", :latitude=>1.1, :longitude=>1.1, :floor=>"1", :roomNumber=>"101", :description=>"This is a test location")
+    Location.create(:name=>"Woodward", :latitude=>1.1, :longitude=>1.1, :floor=>"2", :roomNumber=>"200", :description=>"This is a test location")
+    Location.create(:name=>"Cone", :latitude=>2.2, :longitude=>2.2, :floor=>"1", :roomNumber=>"100", :description=>"This is a test location")
+    Location.create(:name=>"Cone", :latitude=>2.2, :longitude=>2.2, :floor=>"1", :roomNumber=>"101", :description=>"This is a test location")
+    Location.create(:name=>"Kennedy", :latitude=>3.3, :longitude=>3.3, :floor=>"1", :roomNumber=>"100", :description=>"This is a test location")
