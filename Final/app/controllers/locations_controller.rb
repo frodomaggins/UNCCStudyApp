cc0be33545a7f4f3bad01f38ed943d09795e1887
Andrew Davis <adavi@uncc.edu>
2018-04-25 02:33:09 +0000

    Added a search feature

--- Final/app/controllers/locations_controller.rb
+++ Final/app/controllers/locations_controller.rb
@@ -1,6 +1,13 @@
 class LocationsController < ApplicationController
     def index
-        # @location = Location.all
+        @bool = 0
+        @location = Location.all
+        if params[:search]
+            @bool = 1
+            @location = Location.search(params[:search]).order("created_at DESC")
+        else
+            @location = Location.all.order("created_at DESC")
+        end
         @l1 = Location.where("name = 'Woodward'")
         @l2 = Location.where("name = 'Cone'")
         @l3 = Location.where("name = 'Kennedy'")
--- Final/app/models/location.rb
+++ Final/app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ApplicationRecord
     has_many :comments
     has_many :ratings
+    def self.search(search)
+        where("name LIKE ? OR floor LIKE ? OR description LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+    end
 end
