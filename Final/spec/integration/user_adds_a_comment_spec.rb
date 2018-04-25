dcfdd4dad874a5c9887c80c3cd18187587fe1cc2
Joey <jlogan17@uncc.edu>
2018-04-25 17:24:50 +0000

    working on tests still

--- Final/spec/integration/user_adds_a_comment_spec.rb
+++ Final/spec/integration/user_adds_a_comment_spec.rb
@@ -12,17 +12,17 @@ feature "Commenter adds a comment" do
      #   expect(page).to have_field("Body")
     end
     
-   # scenario "User adds a comment" do
-    #    visit locations_path()
-    #    expect(page).to have_content("Woodward")
-    #    click_link "Show"
-    #    expect(page).to have_content("Comments")
-    #    fill_in "Commenter", with: "John Doe"
-    #    fill_in "Body", with: "Testing comment 2"
-    #    click_button "Comment"
-    #    expect(page).to have_content("John Doe")
-    #    expect(page).to have_content("Testing comment 2")
-    #end
+    scenario "User adds a comment" do
+        visit('/locations/1')
+        expect(page).to have_content("Woodward")
+        click_link "Show"
+        expect(page).to have_content("Comments")
+        fill_in "Commenter", with: "John Doe"
+       fill_in "Body", with: "Testing comment 2"
+        click_button "Comment"
+        expect(page).to have_content("John Doe")
+        expect(page).to have_content("Testing comment 2")
+    end
     
     scenario "User uses the search bar" do
         visit root_path
