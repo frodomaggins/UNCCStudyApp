7cae96e564fd48eee523c48a28a33b4b8a93d38e
Joey <jlogan17@uncc.edu>
2018-04-26 19:26:49 +0000

    Added a test for going to the ABout Us page

--- Final/spec/integration/user_adds_a_comment_spec.rb
+++ Final/spec/integration/user_adds_a_comment_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper.rb'
 
-feature "Commenter adds a comment" do
+feature "Testing User options" do
     scenario "User navigates from the home page to the comments page and adds a comment" do
         visit root_path
         expect(page).to have_content("Welcome")
@@ -12,21 +12,28 @@ feature "Commenter adds a comment" do
      #   expect(page).to have_field("Body")
     end
     
-    scenario "User adds a comment" do
+  #  scenario "User adds a comment" do
+  #      visit root_path
+  #      expect(page).to have_content("Welcome")
+  #      click_link "Locations"
+  #      expect(page).to have_content("Locations")
+  #      visit location_url(1)
+     #   gets('/locations/1').should route_to("Location", :id => "1")
+  #      expect(page).to have_content("Woodward")
+  #      click_link "Show"
+  #      expect(page).to have_content("Comments")
+  #      fill_in "Commenter", with: "John Doe"
+  #      fill_in "Body", with: "Testing comment 2"
+  #      click_button "Comment"
+  #      expect(page).to have_content("John Doe")
+  #      expect(page).to have_content("Testing comment 2")
+  #  end
+  
+    scenario "User goes to the about us page" do
         visit root_path
         expect(page).to have_content("Welcome")
-        click_link "Locations"
-        expect(page).to have_content("Locations")
-        visit location_url(1)
-     #   gets('/locations/1').should route_to("Location", :id => "1")
-        expect(page).to have_content("Woodward")
-        click_link "Show"
-        expect(page).to have_content("Comments")
-        fill_in "Commenter", with: "John Doe"
-        fill_in "Body", with: "Testing comment 2"
-        click_button "Comment"
-        expect(page).to have_content("John Doe")
-        expect(page).to have_content("Testing comment 2")
+        click_link "About Us"
+        expect(page).to have_content("Emails")
     end
     
     scenario "User uses the search bar" do
