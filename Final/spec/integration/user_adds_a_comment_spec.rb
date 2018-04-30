6ebb5dfe9af18a862bb8137dbfaffe910c29a38b
Joey <jlogan17@uncc.edu>
2018-04-30 02:03:42 +0000

    Cleaning up the RSPEC tests

--- Final/spec/integration/user_adds_a_comment_spec.rb
+++ Final/spec/integration/user_adds_a_comment_spec.rb
@@ -1,34 +1,14 @@
 require 'rails_helper.rb'
 
 feature "Testing User options" do
-    scenario "User navigates from the home page to the comments page and adds a comment" do
+    scenario "User navigates from the home page to the locations page" do
         visit root_path
         expect(page).to have_content("Welcome")
         click_link "Locations"
-        expect(page).to have_content("Locations")
-     #   click_link "Show"
-     #   expect(page).to have_content("Comments")
-     #   expect(page).to have_field("Commenter")
-     #   expect(page).to have_field("Body")
+        expect(page).to have_content("Atkins Library")
     end
     
- #   scenario "User adds a comment" do
- #       visit root_path
- #       expect(page).to have_content("Welcome")
- #       click_link "Locations"
- #       expect(page).to have_content("Locations")
- #          # breaks rspec   gets('locations/1').should route_to("Location", :id=> 1)
- #       visit('locations/1')
- #       expect(page).to have_content("Woodward")
- #       # click_link "Show"
- #       expect(page).to have_content("Comments")
- #       fill_in "Commenter", with: "John Doe"
- #       fill_in "Body", with: "Testing comment 2"
- #       click_button "Comment"
- #       expect(page).to have_content("John Doe")
- #       expect(page).to have_content("Testing comment 2")
- #   end
-  
+
     scenario "User goes to the about us page" do
         visit root_path
         expect(page).to have_content("Welcome")
@@ -42,5 +22,22 @@ feature "Testing User options" do
         fill_in "Search Locations", with: "cone"
         click_button "Search"
         expect(page).to have_content("Searches")
+    end
+    
+        scenario "User adds a comment" do
+        visit root_path
+        expect(page).to have_content("Welcome")
+        click_link "Locations"
+        expect(page).to have_content("Locations")
+           # breaks rspec   gets('locations/1').should route_to("Location", :id=> 1)
+        visit('locations/1')
+        expect(page).to have_content("Woodward")
+        # click_link "Show"
+        expect(page).to have_content("Comments")
+        fill_in "Commenter", with: "John Doe"
+        fill_in "Body", with: "Testing comment 2"
+        click_button "Comment"
+        expect(page).to have_content("John Doe")
+        expect(page).to have_content("Testing comment 2")
     end
 end
